f40ed742fb2d4e5baddea0c4b4b6e881fba5411d
RTEYL <rteyl1690@gmail.com>
2020-07-18 16:51:33 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  # def signin
+  #   @users = User.all
+  #   @user = User.new
+  # end
+
+  # def login
+  #   @user = User.find_by(name: params[:name])
+  #   if @user && @user.authenticate(password: params[:password])
+  #     session[:user_id] = @user.id
+  #     redirect_to user_path(@user)
+  #   end
+  # end
+
+  # def signout
+  #   session.delete :user_id
+  #   redirect_to root_path
+  # end
+
+end
